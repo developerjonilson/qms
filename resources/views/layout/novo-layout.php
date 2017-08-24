7be12192df61dc1262ff899a216ed9021c403ad3
Jonilson <jonilsons9@gmail.com>
2017-08-24 14:39:24 -0300

    blade construindo

--- resources/views/layout/novo-layout.php
+++ resources/views/layout/novo-layout.php
@@ -114,62 +114,9 @@
 			<!-- MAIN CONTENT -->
 			<div class="main-content">
 				<div class="container-fluid">
-					<h3 class="page-title">Agendar consulta</h3>
-					<div class="row">
-						<div class="col-md-12">
-							<!-- PANEL HEADLINE -->
-							<div class="panel panel-headline">
-								<div class="panel-heading">
-									<h3 class="panel-title">Dados do paciente</h3>
-									<p class="panel-subtitle">O campo abaixo é obrigatórios</p>
-								</div>
-								<div class="panel-body">
-									<div class="row">
-										<div class="col-md-4">
-											<h4>Informe o CNS:</h4><input class="form-control" type="number" name="" value="" placeholder="">
-										</div>
-									</div><br>
-                  <div class="row">
-                    <div class="col-md-3">
-                      <button type="button" class="btn btn-success"><i class="fa fa-search"></i> Buscar</button>
-                    </div>
-                  </div>
-                  <div class="row">
-										<div class="col-md-4">
-											<h4>Paciente:</h4><div class="form-control">Carlos Henrique Matias</div>
-									  </div>
-								  </div>
-									<div class="row">
-										<div class="col-md-4">
-											<h4>Data da consulta:</h4><input class="form-control" type="date" name="" value="">
-									  </div>
-								  </div>
-									<div class="row">
-										<div class="col-md-4">
-											<h4>Especialidade:</h4>
-											<select class="form-control">
-												<option value="cheese">Selecione</option>
-												<option value="tomatoes"></option>
-												<option value="mozarella"></option>
-											</select>
-									  </div>
-								  </div>
-									<div class="row">
-										<div class="col-md-10">
-											<h4>Nome do médico:</h4><input class="form-control" type="text" name="" value="">
-									  </div>
-								  </div><br>
-									<div class="row">
-                    <div class="col-md-3">
-                      <button type="button" class="btn btn-success"><i class="fa fa-calendar"></i> Agendar consulta</button>
-                    </div>
-                  </div>
-								</div>
-							</div>
-							<!-- END PANEL HEADLINE -->
-						</div>
 
-					</div>
+				@yield('conteudo')
+
 				</div>
 			</div>
 			<!-- END MAIN CONTENT -->
